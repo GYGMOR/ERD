7a6765fac05e1f2e8d2e9c72200849d66eeeeac2
Joel Hediger <joel.hediger@sonnenberg-baar.ch>
2026-04-28 11:07:22 +0200

    fix: express 5 named parameter for wildcard

--- server/index.ts
+++ server/index.ts
@@ -1719,7 +1719,7 @@ app.post('/api/tickets/:id/messages', authenticateToken, authorizeRole('admin', 
 });
 
 // For any other request, serve the index.html (Client Side Routing)
-app.get('(.*)', (req, res) => {
+app.get('/:any*', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
